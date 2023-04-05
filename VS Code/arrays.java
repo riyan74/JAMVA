2438bc73ca840e0c1b5404850142808532925fae
riyan <riyann74@gmail.com>
2023-04-05 14:58:53 +0530

    1/4/23

--- VS Code/arrays.java
+++ VS Code/arrays.java
@@ -70,6 +70,8 @@ public class arrays{
 
 */
 
+/* 
+
 //    changing value using function of array
 
 import java.util.Arrays;
@@ -93,5 +95,140 @@ public class arrays{
  
     static void changevale(int[] arr){
         arr[0] = 82;
+    }
+}
+
+*/
+
+//  SYNTAX OF 2D ARRAY
+/* 
+
+   int[][] arr = new int[3][3];
+   (or)
+   int[][] arr = {
+        {1,2,3};
+        {4,5,6};
+        {7,8,9};
+   };
+
+*/
+
+
+/* 
+
+import java.util.Scanner;
+
+public class arrays{
+    public static void main(String[] args) {
+        int[][] arr = new int[3][3];
+        
+         //  input in 2D array
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter elements: ");
+        for(int i=0;i<arr.length;i++){             //   for(int row=0;row<arr.length;row++){     
+            for(int j=0;j<arr[i].length;j++){      //      for(int column=0;j<arr[row].length;column++){
+                arr[i][j]=sc.nextInt();            //         arr[row][column]=sc.nextInt();
+            }                                      //      }
+        }                                          //   }
+    
+         //  output in 2D array
+
+        System.out.println("The matrix is ");
+        for(int i=0;i<arr.length;i++){               
+            for(int j=0;j<arr[i].length;j++){      
+                System.out.print(arr[i][j] + " ");          
+            }   
+            System.out.println();                                   
+        }     
+                                         
+    }
+}
+
+*/
+
+/*
+ 
+//      ArrayList(we can use when the size is not known)
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class arrays{
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(5);
+
+        Scanner sc = new Scanner(System.in);
+        
+        for(int i = 0; i< 5 ; i++){
+            list.add(sc.nextInt());
+        }
+
+        System.out.println(list);
+
+    }
+} 
+*/
+/* 
+//           swap index values
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class arrays{
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter array elements: ");
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        System.out.print(" Enter Index 1 to Swap:");
+        int k = sc.nextInt();
+        System.out.print(" Enter Index 2 to Swap:");
+        int j = sc.nextInt();
+
+        
+        swap(arr,k,j);
+        System.out.println(Arrays.toString(arr));
+        
+    }
+
+    static void swap(int[] arr,int k,int j){
+        int temp;
+        temp = arr[k];
+        arr[k] = arr[j];
+        arr[j] = temp;
+    }
+}
+*/
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class arrays{
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter array elements: ");
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        System.out.println(max(arr));
+        
+        
+    }
+
+    static int max(int[] arr){
+        int max=arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max = arr[i];
+            }
+        }
+        return max;
     }
 }
